52d3f5997c1ddaeef9176e929ac6e92772edda98
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-16 23:05:30 -0400

    fix tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import("tailwindcss").Config} */
+module.exports={darkMode:["class"],content:["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],theme:{extend:{colors:{border:"hsl(var(--border,214.3 31.8% 91.4%))"},borderRadius:{lg:"0.5rem"}}},plugins:[]}
